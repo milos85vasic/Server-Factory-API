228a3033e4c6ab3dfd8997deb45bf7384945f429
Milos Vasic <milos85vasic@gmail.com>
2020-10-10 19:39:20 +0200

    issues/4 - Formatting.

--- src/main/kotlin/com/example/serverFactoryAPI/security/jwt/JwtUtils.kt
+++ src/main/kotlin/com/example/serverFactoryAPI/security/jwt/JwtUtils.kt
@@ -10,12 +10,20 @@ import java.util.*
 
 @Component
 class JwtUtils {
+
+    companion object {
+
+        private val logger = LoggerFactory.getLogger(JwtUtils::class.java)
+    }
+
     @Value("\${mailServerFactoryAPI.app.jwtSecret}")
     private val jwtSecret: String? = null
 
     @Value("\${mailServerFactoryAPI.app.jwtExpirationMs}")
     private val jwtExpirationMs = 0
+
     fun generateJwtToken(authentication: Authentication): String {
+
         val userPrincipal = authentication.principal as UserDetailsImplementation
         return Jwts.builder()
                 .setSubject(userPrincipal.username)
@@ -26,28 +34,32 @@ class JwtUtils {
     }
 
     fun getUserNameFromJwtToken(token: String?): String {
+
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).body.subject
     }
 
     fun validateJwtToken(authToken: String?): Boolean {
+
         try {
+
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken)
             return true
         } catch (e: SignatureException) {
+
             logger.error("Invalid JWT signature: {}", e.message)
         } catch (e: MalformedJwtException) {
+
             logger.error("Invalid JWT token: {}", e.message)
         } catch (e: ExpiredJwtException) {
+
             logger.error("JWT token is expired: {}", e.message)
         } catch (e: UnsupportedJwtException) {
+
             logger.error("JWT token is unsupported: {}", e.message)
         } catch (e: IllegalArgumentException) {
+
             logger.error("JWT claims string is empty: {}", e.message)
         }
         return false
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(JwtUtils::class.java)
     }
 }
